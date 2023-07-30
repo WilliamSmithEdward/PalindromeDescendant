bbbdd27c59d7374a5df2aeb477f85dc1c5a8727c
willi <willi@DESKTOP-8M9VJFP>
2023-07-29 17:29:17 -0700

    Add project files.

---
+++ PalindromeDescendant/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace PalindromeDescendant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PalindromeDescendant(10210112));
+        }
+
+        public static bool PalindromeDescendant(int num)
+        {
+            string numAsString = num.ToString();
+
+            bool palindromeFound = false;
+
+            while (true)
+            {
+                string newNumAsString = "";
+
+                if (numAsString.Length > 2)
+                {
+                    string firstHalfOfNumAsString = numAsString.Substring(0, numAsString.Length / 2);
+                    Console.WriteLine("First half of num: " + firstHalfOfNumAsString);
+
+                    string secondHalfOfNumAsString = numAsString.Substring(numAsString.Length / 2, numAsString.Length / 2);
+                    Console.WriteLine("Second half of num reversed: " + string.Join("", secondHalfOfNumAsString.Reverse()));
+
+                    if (firstHalfOfNumAsString.Equals(string.Join("", secondHalfOfNumAsString.Reverse()))) { palindromeFound = true; break; }
+
+                    for (int i = 0; i < numAsString.Length; i = i + 2)
+                    {
+                        if (i + 2 < numAsString.Length || numAsString.Length % 2 == 0)
+                        {
+                            newNumAsString += (int.Parse(numAsString[i].ToString()) + int.Parse(numAsString[i + 1].ToString())).ToString();
+                        }
+                    }
+
+                    numAsString = newNumAsString;
+                }
+
+                else if (numAsString.Length == 2)
+                {
+                    Console.WriteLine("Two digit num: " + numAsString);
+
+                    if (numAsString.Substring(0, 1).Equals(numAsString.Substring(1, 1)))
+                    {
+                        palindromeFound = true;
+                    }
+
+                    break;
+                }
+
+                else
+                {
+                    break;
+                }
+            }
+
+            return palindromeFound;
+        }
+    }
+}
